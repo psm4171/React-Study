2010a6fa17efb0de3e35a077b1b0dd6c208e6c49
kkjjhh4171@naver.com <kkjjhh4171@naver.com>
2023-04-06 03:48:45 +0900

    useState 사용

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'; // 변수가 변하더라도 자동으로 재렌더링 
 
 function App() {
 
-  let post = '의류 브랜드'; 
+  let posts = '의류 브랜드'; // 자료 잠깐 저장 let 
+  let [글제목, b] = useState(['리액트','자바스크립트','프론트엔드']); // useState(보관할 자료) 
+ 
+
+
 
   return (
     <div className="App">
       <div className="black-nav">
-      <div>
-        <h4>안녕하세요. </h4>
-        <h3 style = {{color: 'red', fontSize: '16px'}} >승민의 BLOG</h3>
+
+         <h3 style = {{color: 'white', fontSize: '16px'}} >승민's 블로그</h3> 
       </div>
 
-      <h4> {post}</h4>
-    
-      
+      <div className="list">
+        <h4> { 글제목[0] } </h4>
+        <p> 4.6일</p>
+      </div>
+
+      <div className="list">
+        <h4> { 글제목[1] } </h4>
+        <p> 6.12일</p>
+      </div>
+
+      <div className="list">
+        <h4> { 글제목[2] } </h4>
+        <p> 5.5일</p>
+      </div>
+
     </div>
-    </div>
+
   );
 }
 
