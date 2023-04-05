3cc2722b56cc2a3d5209606f07ad63b7f2259a1a
kkjjhh4171@naver.com <kkjjhh4171@naver.com>
2023-04-06 01:52:54 +0900

    react test

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  let post = '의류 브랜드'; 
+
+  return (
+    <div className="App">
+      <div className="black-nav">
+      <div>
+        <h4>안녕하세요. </h4>
+        <h3 style = {{color: 'red', fontSize: '16px'}} >승민의 BLOG</h3>
+      </div>
+
+      <h4> {post}</h4>
+    
+      
+    </div>
+    </div>
+  );
+}
+
+export default App;
