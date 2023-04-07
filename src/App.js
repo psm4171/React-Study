fd96e463c8be4df6197e157586a63581c0a26163
kkjjhh4171@naver.com <kkjjhh4171@naver.com>
2023-04-07 18:34:08 +0900

    state 값 변경

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import { useState } from 'react'; // 변수가 변하더라도 자동으로 재�
 
 function App() {
 
-  let posts = '의류 브랜드'; // 자료 잠깐 저장 let 
   let [글제목, b] = useState(['리액트','자바스크립트','프론트엔드']); // useState(보관할 자료) 
- 
+   //let [따봉, 따봉변경] = useState(0); // c는 state 변경 함수 
+   let [따봉, 따봉변경] = useState(0);
+
 
 
 
@@ -18,7 +19,8 @@ function App() {
       </div>
 
       <div className="list">
-        <h4> { 글제목[0] } </h4>
+      <h4> {글제목[0]} <span onClick ={ () =>{따봉변경(따봉+1) }}>👍</span> 
+      {따봉} </h4>
         <p> 4.6일</p>
       </div>
 
