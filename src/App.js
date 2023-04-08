bbb77c99a5a093b7129965cc22f228250b3d0a1a
kkjjhh4171@naver.com <kkjjhh4171@naver.com>
2023-04-08 23:58:51 +0900

    array와 object state 변경 방법

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useState } from 'react'; // 변수가 변하더라도 자동으로 재�
 
 function App() {
 
-  let [글제목, b] = useState(['리액트','자바스크립트','프론트엔드']); // useState(보관할 자료) 
+  let [글제목, 글제목변경] = useState(['리액트','자바스크립트','프론트엔드']); // useState(보관할 자료) 
    //let [따봉, 따봉변경] = useState(0); // c는 state 변경 함수 
    let [따봉, 따봉변경] = useState(0);
 
@@ -17,6 +17,13 @@ function App() {
 
          <h3 style = {{color: 'white', fontSize: '16px'}} >승민's 블로그</h3> 
       </div>
+
+      <button onClick={()=> {
+       // 글제목[0] = 'Node js'; 이렇게 원본 데이터에서 수정하는 것보다 
+       let copy = [...글제목];  // [...] 이렇게 작성해야 함 -> state 변경함수는 
+       copy[0] = 'Node js'; 
+        글제목변경(copy); // 기존 state 값을 신규 state인 copy로 갈아치움, 
+      }}>글 수정</button>
 
       <div className="list">
       <h4> {글제목[0]} <span onClick ={ () =>{따봉변경(따봉+1) }}>👍</span> 
