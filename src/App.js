fcab5f2f7232344e1f2d1e84a661eaa812736f7f
kkjjhh4171@naver.com <kkjjhh4171@naver.com>
2023-04-09 01:39:06 +0900

    array, object state 변경 방법 2

--- src/App.js
+++ src/App.js
@@ -4,12 +4,8 @@ import { useState } from 'react'; // 변수가 변하더라도 자동으로 재�
 
 function App() {
 
-  let [글제목, 글제목변경] = useState(['리액트','자바스크립트','프론트엔드']); // useState(보관할 자료) 
-   //let [따봉, 따봉변경] = useState(0); // c는 state 변경 함수 
-   let [따봉, 따봉변경] = useState(0);
-
-
-
+  let [글제목, 글제목변경] = useState(['리액트','자바스크립트','Node Js']); // useState(보관할 자료) 
+  let [따봉, 따봉변경] = useState(0);
 
   return (
     <div className="App">
@@ -18,32 +14,40 @@ function App() {
          <h3 style = {{color: 'white', fontSize: '16px'}} >승민's 블로그</h3> 
       </div>
 
-      <button onClick={()=> {
-       // 글제목[0] = 'Node js'; 이렇게 원본 데이터에서 수정하는 것보다 
-       let copy = [...글제목];  // [...] 이렇게 작성해야 함 -> state 변경함수는 
-       copy[0] = 'Node js'; 
-        글제목변경(copy); // 기존 state 값을 신규 state인 copy로 갈아치움, 
-      }}>글 수정</button>
+      
+      {/* // 글제목[0] = 'Node js'; 이렇게 원본 데이터에서 수정하는 것보다 
+      // [...] 이렇게 작성해야 함 -> state 변경함수가 새로 적용됨 
+       // [...] 이렇게 작성해야 함 -> state 변경함수가 새로 적용됨 
+       // 기존 state 값을 신규 state인 copy로 갈아치움, 기존 state가 arr/object면 독립적 copy를 만들어서 수정해야 함
+       
+      // 기존 state 값을 신규 state인 copy로 갈아치움, 기존 state가 arr/object면 독립적 copy를 만들어서 수정해야 함 */}
+      
+  
+     <button onClick={()=> {
 
-      <div className="list">
-      <h4> {글제목[0]} <span onClick ={ () =>{따봉변경(따봉+1) }}>👍</span> 
-      {따봉} </h4>
-        <p> 4.6일</p>
+      let copy = [...글제목]; 
+      copy.sort(); 
+      글제목변경(copy)
+     }}>'가나다순 정렬'</button>
+
+
+     <div className="list">
+      <h4> { 글제목[0] } <button onClick={ ()=>{ 따봉변경(따봉 + 1) } } >좋아요👍</button> { 따봉 }</h4>
+      <p> 4.19일 </p>
       </div>
 
       <div className="list">
-        <h4> { 글제목[1] } </h4>
+        <h4> { 글제목[1] } <button onClick={ ()=>{ 따봉변경(따봉 + 1) } } >좋아요👍</button> { 따봉 }</h4>
         <p> 6.12일</p>
       </div>
 
       <div className="list">
-        <h4> { 글제목[2] } </h4>
+        <h4> { 글제목[2] } <button onClick={ ()=>{ 따봉변경(따봉 + 1) } } >좋아요👍</button> { 따봉 }</h4>
         <p> 5.5일</p>
       </div>
+    
+</div>
 
-    </div>
-
-  );
+ );
 }
-
 export default App;
