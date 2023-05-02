c21d6c4469f2bfee723ab6a1932a624a1cc32e23
kkjjhh4171@naver.com <kkjjhh4171@naver.com>
2023-05-02 20:31:16 +0900

    동적인 UI 모달창 생성 재검토

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   let [따봉, 따봉변경] = useState([0,0,0,0]);
 
   let [modal, setModal] = useState(false); // ui상태 열림true/닫힘false 
-  let [title] = useState(0); //0이면 0번째 제목 
+  let [title, setTitle] = useState(0); //0이면 0번째 제목 
   // map 함수를 이용하여 반복 작업 줄임 
 
   return (
@@ -16,6 +16,7 @@ function App() {
       <div className="black-nav">
 
          <h3 style = {{color: 'white', fontSize: '16px'}} >승민's 블로그</h3> 
+        
       </div>
       
       {/* // 글제목[0] = 'Node js'; 이렇게 원본 데이터에서 수정하는 것보다 
@@ -26,65 +27,60 @@ function App() {
       // 기존 state 값을 신규 state인 copy로 갈아치움, 기존 state가 arr/object면 독립적 copy를 만들어서 수정해야 함 */}
       
   
-     <button onClick={()=> {
+      <button onClick={()=> {
 
       let copy = [...글제목]; 
       copy.sort(); 
       글제목변경(copy)
-     }}>'가나다순 정렬'</button>
+     }}>'가나다순 정렬'</button> 
 
 
-     {/* <div className="list">
-      <h4> { 글제목[0] } <button onClick={ ()=>{ 따봉변경(따봉 + 1) } } >좋아요👍</button> { 따봉 }</h4>
-      <p> 4.19일 </p>
-      </div>
 
-      <div className="list">
-        <h4> { 글제목[1] } <button onClick={ ()=>{ 따봉변경(따봉 + 1) } } >좋아요👍</button> { 따봉 }</h4>
-        <p> 6.12일</p>
-      </div>
 
-      <div className="list">
-        <h4 onClick={()=> {setModal(!modal) } }> { 글제목[2] } <button onClick={ ()=>{ 따봉변경(따봉 + 1) } } >좋아요👍</button> { 따봉 }</h4>
+      {/* <button onClick={()=>{setTitle(0)}}> 글제목0</button>
+      <button onClick={()=>{setTitle(1)}}> 글제목1</button>
+      <button onClick={()=>{setTitle(2)}}> 글제목2</button> */}
 
-        <p> 5.5일</p>
-      </div> */}
+     
       
-      {
-      (modal == true ? <Modal title={title} 글제목={글제목}/> : null)
-        }
+      
 
-{
+
+{ // array 갯수만큼 반복  
   // 반복 생성한 ui마다 각각의 다른 key값을 설정해줘야 함
   // map 반복문으로 반복생성한 html엔 key={i} 이런 속성을 추가해야 함.
+  // a라는 파라미터는 map이 반복될 때마다 array자료 안에 있던 하나하나의 데이터들을 의미
           글제목.map(function(a, i){
             return (
-
               <div className="list" key={i}> 
-            <h4 onClick={()=> {setModal(!modal) } }></h4>
-                
+            <h4 onClick={()=> {setModal(!modal) } }>{글제목[i]} </h4> 
                 <h4>
-                  {글제목[i]}
                   <button onClick={()=> {
                   let copy = [...따봉];
                   copy[i] = copy[i]+ 1; 
                   따봉변경(copy)
                   }}>좋아요👍</button> {따봉[i]}
                 </h4>
+                 <p> 6.12일</p> 
 
-            <p> 6.12일</p>
+              
+
              </div>
-
-             
-
             )
           })
         }
-      
+
+{
+          modal == true ? <Modal/> : null 
+        }
+       
+        
 </div>
 
+      
  );
 }
+
 
 // 컴포넌트 - 모달 함수, 다른 함수 바깥에 만들것 
 // 컴포넌트는 반복적인 html 축약할 때 사용 
@@ -92,15 +88,25 @@ function App() {
 // 자주 변경되는 것들
 
 
-function Modal(props){ // props로 전송 
+// function Modal(props){ // props로 전송 
 
-  return(
+//   return(
+//     <div className="modal">
+//         <h4>{props.글제목[props.title]}</h4> 
+//         <p>날짜</p>
+//         <p>상세내용</p>
+//         <button onClick={()=> props.글제목변경(['리액트를 활용한 웹 개발'])} >글수정</button>
+//       </div>
+//   )
+// }
+
+function Modal(props){
+  return (
     <div className="modal">
-        <h4>{props.글제목[props.title]}</h4> 
-        <p>날짜</p>
-        <p>상세내용</p>
-        <button onClick={()=> props.글제목변경(['리액트를 활용한 웹 개발'])} >글수정</button>
-      </div>
+      <h4>글제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
   )
 }
 
