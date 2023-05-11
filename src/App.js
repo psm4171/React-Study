4607ba9adbe32d5fb596d1b3b8ad0cffcf04c004
kkjjhh4171@naver.com <kkjjhh4171@naver.com>
2023-05-11 18:40:47 +0900

    input_사용자가 입력한 값

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   let [modal, setModal] = useState(false); // ui상태 열림true/닫힘false 
   let [title, setTitle] = useState(0); //0이면 0번째 제목 
   // map 함수를 이용하여 반복 작업 줄임 
+  let [입력값, 입력값변경] = useState(''); // 유저가 입력한 값을 input에 저장 
 
   return (
     <div className="App">
@@ -38,12 +39,17 @@ function App() {
   // 반복 생성한 ui마다 각각의 다른 key값을 설정해줘야 함
   // map 반복문으로 반복생성한 html엔 key={i} 이런 속성을 추가해야 함.
   // a라는 파라미터는 map이 반복될 때마다 array자료 안에 있던 하나하나의 데이터들을 의미
-          글제목.map(function(a, i){
+  // <span onClick={(e) => { e.stopPropagation(); } 해주면 이벤트버블링(상위 태그까지 실행되는) 해결 
+  
+  // 입력값변경(e.target.value); 
+  // state 변경함수 작업은 늦게 처리되기 때문에 console.log(입력값)을 먼저 실행
+  // 따라서 a 값은 출력 안되고 aa값부터 출력 
+  글제목.map(function(a, i){
             return (
               <div className="list" key={i}> 
             <h4 onClick={()=> {setModal(!modal); setTitle(i) } }>{글제목[i]} </h4> 
                 <h4>
-                  <button onClick={()=> {
+                  <button onClick={(e)=> { 
                   let copy = [...따봉];
                   copy[i] = copy[i]+ 1; 
                   따봉변경(copy)
@@ -58,8 +64,11 @@ function App() {
           })
         }
 
+
+       <input onChange={(e)=>{입력값변경(e.target.value); 
+      console.log(입력값) }} />
 {
-        //   modal == true ? <Modal/> : null 
+        
         modal == true ? <Modal title={title} 글제목={글제목}/> : null 
         }
        
@@ -69,6 +78,10 @@ function App() {
       
  );
 }
+
+// EventHandler --> onChange 값 입력때마다 console.log(값) 출력 
+//onMouseOver 마우스 갔다대면 값 출력
+//onScroll 스크롤 할 때마다 값을 출력 
 
 
 // 컴포넌트 - 모달 함수, 다른 함수 바깥에 만들것 
